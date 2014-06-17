3ad97ed79eae29df7918a3648610d0f5eba6a232
nnegrey <noah.negrey@gmail.com>
2014-06-17 10:05:26 -0700

    Cleaning up files

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,7 +41,7 @@ describe "Static pages" do
 
   describe "About page" do
 
-    it "should ahve the content 'About Us'" do
+    it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
@@ -51,4 +51,18 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  # Contatct page test
+  describe "Contact Page" do
+    it "should have the content 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_content('Contact')
+    end
+
+    it "should have the content 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact");
+    end
+  end
 end
+
