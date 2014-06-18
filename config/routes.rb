797b2589cda5c2a8dadd6104e04d06e1f05385d9
nnegrey <noah.negrey@gmail.com>
2014-06-17 17:13:26 -0700

    Added simple user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
   #get "static_pages/..." # MY EDIT
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root  'static_pages#home'
-  match '/signup', to: 'users#new', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
